1278080264eb5c723bc9a51f8d25c57d341dd9dd
xXJuanDavidXx <juandavidjaramillo456@gmail.com>
2024-08-04 20:21:41 +0000

    Catalogo de productos con mensaje a whatsapp del poducto de interes(Para usar modificar a conveniencia)

---
+++ app/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Locion, Profile
+
+# Register your models here.
+
+admin.site.register(Locion)
+admin.site.register(Profile)
+
+
+
+
---
+++ app/urls.py
@@ -0,0 +1,21 @@
+
+from django.urls import path,include
+from . import views
+from django.contrib.auth import views as auth_views
+from .views import SignUpView, CustomLoginView
+
+urlpatterns = [
+    path('catalogo/<int:user_id>/', views.lista_lociones, name='catalogo'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('profile/edit/', views.profile_edit, name='profile_edit'),
+    path('signup/', SignUpView.as_view(), name='signup'),
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='registration/password_reset.html'), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'), name='password_reset_complete'),
+    path('logout/',views.singout, name='logout'),
+    path('', views.lista_lociones, name='lista_lociones'),
+    path('copiar/', views.copiar, name='copiar')
+
+    ]
