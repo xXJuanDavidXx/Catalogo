445a3289196bb8e611bfbadd5395865f0862632e
xXJuanDavidXx <juandavidjaramillo456@gmail.com>
2024-08-05 16:53:15 +0000

    Problema que no entiedo 2 ;-;

--- app/urls.py
+++ app/urls.py
@@ -3,6 +3,9 @@ from django.urls import path,include
 from . import views
 from django.contrib.auth import views as auth_views
 from .views import SignUpView, CustomLoginView
+from django.conf.urls.static import static
+
+
 
 urlpatterns = [
     path('catalogo/<int:user_id>/', views.lista_lociones, name='catalogo'),
