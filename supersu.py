d82336f206ed6be30b6e361748aa8801d1d61638
xXJuanDavidXx <juandavidjaramillo456@gmail.com>
2024-08-05 00:09:12 +0000

    Corregi otra

--- supersu.py
+++ supersu.py
@@ -1,20 +1,17 @@
 import os
 import django
-from django.conf import settings
-from django.contrib.auth.models import User
+from django.core.management import call_command
 
 # Configura el entorno de Django
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'catalogo.settings')  # Cambia 'miProyecto.settings' por el nombre de tu módulo de configuración
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'catalogo.settings')  # Cambia esto si es necesario
 django.setup()
 
-# Crea el usuario
-user = User.objects.create_user(
+# Ejecuta el comando `createsuperuser` con opciones proporcionadas
+call_command(
+    'createsuperuser',
     username='thegame',
     email='thegame@thegame.com',
-    password='thegame123'
+    password='thegame123',
+    interactive=False
 )
 
-# Asigna permisos de superusuario y miembro del personal
-user.is_superuser = True
-user.is_staff = True
-user.save()
