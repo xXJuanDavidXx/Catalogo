e7cd116ca8b6789b981b794db486bdfbf8c0e58e
xXJuanDavidXx <juandavidjaramillo456@gmail.com>
2024-08-05 00:21:56 +0000

    Diosito ayudame plis aaaa

--- supersu.py
+++ supersu.py
@@ -1,4 +1,14 @@
 from django.contrib.auth.models import User
+import os
+import django
+from django.conf import settings
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'catalogo.settings')  # Cambia esto si es necesario
+django.setup()
+
+
+
 
 # Crea el usuario
 user = User.objects.create_superuser(
